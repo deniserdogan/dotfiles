1f5106c4c29ed932ba3c70a0194af2b788ed0011
deniserdogan <hakandenizerdogan@gmail.com>
2024-02-01 22:10:21 +0300

    gitsigns and jsx autoclose tag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,22 @@
 return {
   {
+    -- Autoclose tags in .jsx
+    "windwp/nvim-ts-autotag",
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = true,
+        -- Enable autotag for .jsx f files
+        autotag = {
+          enable = true,
+        },
         highlight = { enable = true },
         indent = { enable = true },
       })
-    end
-  }
+    end,
+  },
 }
