3fef6c662c1cd0c53bec2622db89f2ead6bd7f42
deniserdogan <hakandenizerdogan@gmail.com>
2024-02-05 20:38:41 +0300

    comment out

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,21 +1,5 @@
 return {
 	{
-		"ramojus/mellifluous.nvim",
-		config = function()
-			require("mellifluous").setup({
-				transparent_background = {
-					enabled = true,
-					floating_windows = true,
-					telescope = true,
-					file_tree = true,
-					cursor_line = true,
-					status_line = false,
-				},
-			}) -- optional, see configuration section.
-		end,
-	},
-
-	{
 		"rebelot/kanagawa.nvim",
 		config = function()
 			-- Default options:
@@ -31,24 +15,40 @@ return {
 		end,
 	},
 
-	{
-		"nyoom-engineering/oxocarbon.nvim",
-	},
-
-	{
-		"embark-theme/vim",
-		as = "embark",
-		config = function() end,
-	},
-
-	{
-		"EdenEast/nightfox.nvim",
-		config = function()
-			require("nightfox").setup({
-				options = {
-					transparent = true,
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"ramojus/mellifluous.nvim",
+	-- 	config = function()
+	-- 		require("mellifluous").setup({
+	-- 			transparent_background = {
+	-- 				enabled = true,
+	-- 				floating_windows = true,
+	-- 				telescope = true,
+	-- 				file_tree = true,
+	-- 				cursor_line = true,
+	-- 				status_line = false,
+	-- 			},
+	-- 		}) -- optional, see configuration section.
+	-- 	end,
+	-- },
+	--
+	-- {
+	-- 	"nyoom-engineering/oxocarbon.nvim",
+	-- },
+	--
+	-- {
+	-- 	"embark-theme/vim",
+	-- 	as = "embark",
+	-- 	config = function() end,
+	-- },
+	--
+	-- {
+	-- 	"EdenEast/nightfox.nvim",
+	-- 	config = function()
+	-- 		require("nightfox").setup({
+	-- 			options = {
+	-- 				transparent = true,
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 }
